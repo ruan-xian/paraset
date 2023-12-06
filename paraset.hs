f0f937b64171325eeaca951c0bd4582fdd029e59
Ryan Xu <ryan-xu@live.com>
2023-12-05 22:41:57 -0500

    single threaded solution

--- paraset.hs
+++ paraset.hs
@@ -1,74 +1,105 @@
-import Data.Set (Set)
-import qualified Data.Set as Set
-
-import Data.Map (Map)
-import qualified Data.Map as Map
-
 import Data.List (sort)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
 
 {-
-cards are represented as lists, where the index represents the trait, and 
+cards are represented as lists, where the index represents the trait, and
 c[i] represents the value of trait i
 -}
 type Card = [Int]
 
-{- 
-main function: given parameters 
-  C: number of cards dealt,  
-  p: number of traits
+{-
+main function: given parameters
+  C: number of cards dealt,
   v: number of values per trait,
+  p: number of traits
 -}
-possibleSetCount :: Int -> Int -> Int -> Int
-possibleSetCount c v p =
-  let
-    dealtCards = dealCards 12 4 3
-    preSets = generatePreSets dealtCards
-  in
-    length $ filter id $ map (\x -> isSetPossible dealtCards x) preSets
-
-
-{- 
-  hard coded for now so we can check 
--}
-dealCards :: Int -> Int -> Int -> [Card]
-dealCards c v p = sort
-  [
-    [3,3,3,1], [2,1,1,2], [1,2,3,2], [2,2,2,3],
-    [1,2,2,3], [3,1,1,3], [2,1,2,2], [3,3,3,3],
-    [2,2,2,1], [1,1,1,2], [1,2,1,1], [2,1,3,2]
-  ]
-
--- https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell 
-generatePreSets :: [Card] -> Int -> [[Card]]
-generatePreSets dealtCards v = generatePreSets' dealtCards v-1
-  where generatePreSets' _ 0 = [[]]
-        generatePresets' [] _ = []
-        generatePresets' (x:xs) n = map (x:) (generatePreSets' xs (n-1)) ++ generatePreSets' xs n
-
+possibleSets :: Int -> Int -> Int -> [[Card]]
+possibleSets c v p =
+  let dealtCards = dealCards c v p
+      preSets = generatePreSets v dealtCards
+   in mapMaybe (getPossibleSet (Set.fromList dealtCards) v) preSets
 
 {-
-  checks if a valid, correctly ordered set is possible 
+solution
+[2,1,1,2]  [2,1,2,2]  [2,1,3,2]
+[1,2,1,1]  [1,2,2,3]  [1,2,3,2]
+[1,2,1,1]  [2,1,2,2]  [3,3,3,3]
+[1,1,1,2]  [2,2,2,3]  [3,3,3,1]
+[1,2,2,3]  [2,1,1,2]  [3,3,3,1]
+[1,1,1,2]  [2,2,2,1]  [3,3,3,3]
+ -}
+
+{-
+code generated solutions
+   [[1,2,1,1],[1,2,2,3],[1,2,3,2]], 2
+   [[1,2,2,3],[2,1,1,2],[3,3,3,1]], 5
+   [[2,1,1,2],[2,1,2,2],[2,1,3,2]], 1
+   [[1,2,1,1],[2,1,2,2],[3,3,3,3]], 3
+   [[1,1,1,2],[2,2,2,1],[3,3,3,3]], 6
+   [[1,1,1,2],[2,2,2,3],[3,3,3,1]]] 4
 -}
-isValidSetPossible :: Set Card -> [Card]  -> Bool
-isValidSetPossible dealtCards preSet =
-  case getMissingCard preSet of
-    Nothing -> False
-    Just missingCard -> member missingCard dealtCards && (isOrderedSet preSet missingCard)
-  where isOrderedSet pS mC = and (zipWith (>=) (head pS) mC)
+
+{-
+  hard coded for now so we can check
+-}
+dealCards :: Int -> Int -> Int -> [Card]
+dealCards c v p =
+  sort
+    [ [3, 3, 3, 1],
+      [2, 1, 1, 2],
+      [1, 2, 3, 2],
+      [2, 2, 2, 3],
+      [1, 2, 2, 3],
+      [3, 1, 1, 3],
+      [2, 1, 2, 2],
+      [3, 3, 3, 3],
+      [2, 2, 2, 1],
+      [1, 1, 1, 2],
+      [1, 2, 1, 1],
+      [2, 1, 3, 2]
+    ]
+
+{-
+https://stackoverflow.com/questions/52602474/function-to-generate-the-unique-combinations-of-a-list-in-haskell
+faster ones here https://stackoverflow.com/questions/26727673/haskell-comparison-of-techniques-for-generating-combinations
+    are not great bc `subsequences` can get really huge if `length dealtCards` is large
+-}
+generatePreSets :: Int -> [Card] -> [[Card]]
+generatePreSets v = generatePreSets' (v - 1)
+  where
+    generatePreSets' 0 _ = [[]]
+    generatePreSets' _ [] = []
+    generatePreSets' n (x : xs) = map (x :) (generatePreSets' (n - 1) xs) ++ generatePreSets' n xs
+
+{-
+  checks if a valid, correctly ordered set is possible
+-}
+getPossibleSet :: Set Card -> Int -> [Card] -> Maybe [Card]
+getPossibleSet dealtCards v preSet@(firstCard : _) =
+  case getMissingCard preSet v of
+    Nothing -> Nothing
+    Just missingCard ->
+      if Set.member missingCard dealtCards && missingCard < firstCard
+        then Just $ missingCard : preSet
+        else Nothing
 
 getMissingValue :: Int -> [Int] -> Maybe Int
 getMissingValue v values@(firstVal : _)
   | Map.size m == 1 = Just firstVal
-  | all (==1) (Map.elems m) = Just $ (v*(v+1) `div` 2) - sum (Map.keys m)
+  | all (== 1) (Map.elems m) = Just $ (v * (v + 1) `div` 2) - sum (Map.keys m)
   | otherwise = Nothing
   where
-      m = Map.fromListWith (+) [(val, 1) | val <- values]
+    m = Map.fromListWith (+) [(val, 1) | val <- values]
 
 getMissingCard :: [Card] -> Int -> Maybe Card
 getMissingCard preSet v =
   mapM (getMissingValue v) transposedList
   where
-    transpose :: [[a]]->[[a]]
-    transpose ([]:_) = []
+    transpose :: [[a]] -> [[a]]
+    transpose ([] : _) = []
     transpose x = map head x : transpose (map tail x)
     transposedList = transpose preSet
