2bd8118147d7d87d6f735725fc164c5b3dd13165
Ryan Xu <ryan-xu@live.com>
2023-12-05 22:42:12 -0500

    oops

--- paraset.hs
+++ paraset.hs
@@ -24,7 +24,7 @@ possibleSets c v p =
    in mapMaybe (getPossibleSet (Set.fromList dealtCards) v) preSets
 
 {-
-solution
+human solution 
 [2,1,1,2]  [2,1,2,2]  [2,1,3,2]
 [1,2,1,1]  [1,2,2,3]  [1,2,3,2]
 [1,2,1,1]  [2,1,2,2]  [3,3,3,3]
