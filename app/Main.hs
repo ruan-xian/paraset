6d939eccdac694267045034aedc021c798dfcbcc
Ryan Xu <ryan-xu@live.com>
2023-12-19 16:02:53 -0500

    command line args

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+import Lib (possibleSets)
+import System.Environment (getArgs, getProgName)
+import System.Exit (die)
 
 main :: IO ()
-main = putStrLn $ possibleSets 12 3 4
+main = do
+  args <- getArgs
+  case args of
+    [c, v, p] -> do
+      print $ possibleSets (read c) (read v) (read p)
+    _ -> do
+      pn <- getProgName
+      die $ "Usage: " ++ pn ++ " <cards dealt> <number of values> <number of traits>"
