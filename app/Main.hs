b1a41093d7cb3f4839f082002fe98b479f5b4cc0
Cindy Zhu <zhucindy1234567890@gmail.com>
2023-12-19 17:18:01 -0500

    possibleSets takes in card arr

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Lib (possibleSets)
+import Lib (dealCardsRandom, possibleSets)
 import System.Environment (getArgs, getProgName)
 import System.Exit (die)
 import System.Random (mkStdGen)
@@ -12,7 +12,8 @@ main = do
     [c, v, p] ->
       do
         let g = mkStdGen 42
-        print $ possibleSets (read c) (read v) (read p) g
+            dealtCards = dealCardsRandom (read c) (read v) (read p) g
+        print $ possibleSets (dealtCards) (read v)
     _ -> do
       pn <- getProgName
       die $ "Usage: " ++ pn ++ " <cards dealt> <number of values> <number of traits>"
