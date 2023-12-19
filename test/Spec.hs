a396b6dc0f892e6b01a41a5d1fa842094bf9978b
Cindy Zhu <zhucindy1234567890@gmail.com>
2023-12-19 17:44:35 -0500

    aded a test for known seq case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,39 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Data.List (sort)
+import Lib (possibleSets)
+import Test.HUnit
+
+testSeq1 :: Test
+testSeq1 =
+  TestCase
+    ( do
+        let dealtCards =
+              sort
+                [ [3, 3, 3, 1],
+                  [2, 1, 1, 2],
+                  [1, 2, 3, 2],
+                  [2, 2, 2, 3],
+                  [1, 2, 2, 3],
+                  [3, 1, 1, 3],
+                  [2, 1, 2, 2],
+                  [3, 3, 3, 3],
+                  [2, 2, 2, 1],
+                  [1, 1, 1, 2],
+                  [1, 2, 1, 1],
+                  [2, 1, 3, 2]
+                ]
+            solution =
+              [ [[1, 2, 1, 1], [1, 2, 2, 3], [1, 2, 3, 2]],
+                [[1, 2, 2, 3], [2, 1, 1, 2], [3, 3, 3, 1]],
+                [[2, 1, 1, 2], [2, 1, 2, 2], [2, 1, 3, 2]],
+                [[1, 2, 1, 1], [2, 1, 2, 2], [3, 3, 3, 3]],
+                [[1, 1, 1, 2], [2, 2, 2, 1], [3, 3, 3, 3]],
+                [[1, 1, 1, 2], [2, 2, 2, 3], [3, 3, 3, 1]]
+              ]
+        assertEqual "" solution $ possibleSets dealtCards 3
+    )
+
+main :: IO Counts
+main =
+  runTestTT $
+    TestList
+      ["Sequential (with 12 3 4)" ~: testSeq1]
